982af1d47a4edf897f68293cfb1895a943dd4115
muskuloes <t.victor@campus.lmu.de>
2020-05-31 18:55:52 +0200

    add file upload dropzone

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import { withStyles, fade } from "@material-ui/core/styles";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridList from "@material-ui/core/GridList";
+import Dropzone from "./Dropzone";
+
+const styles = (theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    position: "absolute",
+    // display: "flex",
+    top: 60,
+  },
+  search: {
+    position: "absolute",
+    display: "flex",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+    border: "solid",
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+});
+
+class ImageGridList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageData: [],
+      query: "",
+      originalData: [],
+    };
+  }
+
+  handleInputChange = (event) => {
+    const query = event.target.value;
+    if (query.length === 0) {
+      this.setState((prevState) => ({
+        ...prevState,
+        imageData: prevState.originalData,
+        query: query,
+      }));
+    } else {
+      this.setState((prevState) => ({
+        ...prevState,
+        imageData: prevState.originalData
+          .sort(() => 0.5 * Math.random()) // shuffle the array of images
+          .slice(0, Math.random() * 29), // 29 is the original size of the data
+        query: query,
+      }));
+    }
+  };
+
+  fetchData = async () => {
+    let response = await fetch(
+      `https://api.unsplash.com/photos/random?client_id=${process.env.REACT_APP_CLIENT_ID}&count=29&query=clothing,fashion,people`
+    );
+    if (response.ok) {
+      let data = await response.json();
+      this.setState({
+        imageData: data,
+        query: "",
+        originalData: data,
+      });
+    } else {
+      console.log(response);
+    }
+  };
+
+  componentDidMount = () => {
+    this.fetchData();
+  };
+
+  render() {
+    const { classes } = this.props;
+    const gridListTiles = [
+      <GridListTile key={"+"} cols={1}>
+        <Dropzone />
+      </GridListTile>,
+      this.state.imageData.map((image) => (
+        <GridListTile key={image.id} cols={image.cols || 1}>
+          <img src={image.urls.small} alt={image.alt_description} />
+        </GridListTile>
+      )),
+    ];
+    return (
+      <div className={classes.root}>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ "aria-label": "search" }}
+            value={this.state.query}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <div className={classes.gridList}>
+          <GridList cellHeight={160} cols={3}>
+            {gridListTiles}
+          </GridList>
+        </div>
+      </div>
+    );
+  }
+}
+export default withStyles(styles)(ImageGridList);
---
+++ src/Dropzone.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { DropzoneDialog } from "material-ui-dropzone";
+import AddIcon from "@material-ui/icons/Add";
+import axios from "axios";
+
+const styles = (theme) => ({
+  addImage: {
+    height: "99%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: theme.shape.borderRadius,
+    color: theme.palette.divider,
+    border: `1px dashed ${theme.palette.divider}`,
+    padding: 0,
+    transition: theme.transitions.create(["color", "border-color"]),
+    "&&": {
+      display: "flex",
+    },
+    "&:hover": {
+      borderColor: "currentColor",
+      color: theme.palette.text.secondary,
+    },
+  },
+});
+class UploadDropzone extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      files: [],
+    };
+  }
+
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  handleSave = (files) => {
+    //Saving files to state for further use and closing Modal.
+    this.setState({
+      files: files,
+      open: false,
+    });
+    const data = new FormData();
+    for (const file of files) {
+      data.append("file", file);
+    }
+    axios
+      .post("http://localhost:5000/upload", data)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  handleOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <>
+        <span
+          className={classes.addImage}
+          rel="noopener noreferrer"
+          target="_blank"
+          onClick={this.handleOpen}
+        >
+          <AddIcon />
+        </span>
+        <DropzoneDialog
+          open={this.state.open}
+          onSave={this.handleSave}
+          acceptedFiles={["image/jpeg", "image/png"]}
+          showPreviews={true}
+          maxFileSize={5000000}
+          onClose={this.handleClose}
+        />
+      </>
+    );
+  }
+}
+export default withStyles(styles)(UploadDropzone);
